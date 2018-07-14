56807a1760bd81fd1cf91b270db225c33a94cbb2
Dagestan05 <steklofan@gmail.com>
2018-07-15 01:36:29 +0200

    Added Error handler on server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
 
+const errorHandler = require("./handlers/error")
+
 const PORT = 8081;
 
 app.use(cors());
@@ -10,13 +12,17 @@ app.use(bodyParser.json());
 
 //ROUTES
 
-
+//Error handler
 app.use(function(req, res, next){
   let err = new Error("Not Found!")
   err.status = 404;
   next(err)
 })
+//Error handler middleware
+app.use(errorHandler);
 
+
+//server
 app.listen(PORT, function(){
   console.log("Server is started on port ", PORT)
 })
