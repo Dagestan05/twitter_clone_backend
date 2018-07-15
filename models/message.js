189e7e3770e52ceaacd41c013aa4b6d5456d82b4
Dagestan05 <steklofan@gmail.com>
2018-07-15 16:25:52 +0200

    created Message model & connected to User model

--- models/message.js
+++ models/message.js
@@ -13,6 +13,24 @@ const messageSchema = new mongoose.Schema({
   }
 });
 
+
+//middlware to remove a message from users messages list
+messageSchema.pre("remove", async function(next) {
+  try {
+    // find a user
+    let user = await user.findById(this.user)
+    // remove the id of the message from thier messages list
+    user.messages.remove(this.id)
+    //save that user
+    await user.save()
+    // return next()
+    return
+  } catch (err) {
+    return next(err)
+  }
+})
+
+
 const Message = mongoose.model("Message", messageSchema);
 
 module.exports = Message;
