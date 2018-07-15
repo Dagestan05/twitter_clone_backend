0529a7a13b637af5a33bfd6416c5e42708062578
Dagestan05 <steklofan@gmail.com>
2018-07-15 02:43:10 +0200

    mongoose setup & created User model

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+mongoose.set('debug', true)// for live watch of mongo queries
+
+mongoose.Promise = Promise // use native es6 promises instead of cbacks
+
+mongoose.connect("mongodb:localhost/twitterclone", {
+  keepAlive: true,
+  useMongoClient: true
+});
